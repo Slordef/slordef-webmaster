f2fa45582b573981897fb050e59279a80ce4f1e0
Maximilien Valenzano <slordef.vikamet@gmail.com>
2025-02-05 22:12:23 +0100

    chore: add open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,40 @@
-import type { Metadata } from "next";
-import "./index.css";
-import Script from "next/script";
+import type { Metadata } from 'next';
+import './index.css';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
-  title: "Slordef | Developer",
-  description: "Personal website of Slordef, a developer.",
-  applicationName: "Slordef",
-  authors: { name: "Slordef", url: "https://slordef-webmaster.ch" },
-  generator: "Slordef",
-  keywords: ["Slordef", "Developer"],
-  referrer: "origin",
-  robots: "index, follow",
+  title: 'Slordef | Developer',
+  description: 'Personal website of Slordef, a developer.',
+  applicationName: 'Slordef',
+  authors: { name: 'Slordef', url: 'https://slordef-webmaster.ch' },
+  generator: 'Slordef',
+  keywords: ['Slordef', 'Developer'],
+  referrer: 'origin',
+  robots: 'index, follow',
   icons: {
-    icon: "/template/logo.png",
+    icon: '/template/logo.png',
+  },
+  openGraph: {
+    type: 'website',
+    locale: 'fr_CH',
+    url: 'https://slordef-webmaster.ch',
+    title: 'Slordef | Developer',
+    description: 'Personal website of Slordef, a developer.',
+    siteName: 'Slordef',
+    images: [
+      {
+        url: '/template/logo.png',
+        width: 367,
+        height: 322,
+        alt: 'Slordef',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@slordef',
+    creator: '@slordef',
+    images: '/template/logo.png',
   },
 };
 
@@ -23,10 +45,7 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <Script
-        src="https://kit.fontawesome.com/c628b2d872.js"
-        crossOrigin="anonymous"
-      />
+      <Script src="https://kit.fontawesome.com/c628b2d872.js" crossOrigin="anonymous" />
       <html lang="fr">
         <body>{children}</body>
       </html>
