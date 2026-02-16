312c3ee6a34fd3eb9acf580f4e9eeba9eca0b9c4
Maximilien Valenzano <slordef.vikamet@gmail.com>
2026-02-16 13:08:26 +0100

    chore: remove old Next.js source files

--- src/components/project/project.tsx
+++
@@ -1,9 +0,0 @@
-import './project.css';
-
-export function Project(props: React.PropsWithChildren) {
-  return (
-    <section id={'project'}>
-      <div className="content">{props.children}</div>
-    </section>
-  );
-}
